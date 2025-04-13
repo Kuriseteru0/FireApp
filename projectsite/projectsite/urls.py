79a3c27fbc68497c9001a1d9054194ed40e073a6
John Victor Dato-on <143370209+johnvictordatoon@users.noreply.github.com>
2025-04-13 23:33:10 +0800

    dynamic charts updated

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountByMonth
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountByMonth, MultilineIncidentTop3Country, multipleBarbySeverity
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
     path('dashboard_chart', ChartView.as_view(), name='dashboard_chart'),
-    path('chart/', PieCountbySeverity, name='chart'),
+    path('pieChart/', PieCountbySeverity, name='chart'),
+    path('lineChart/', LineCountByMonth, name='chart'),
+    path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarChart/', multipleBarbySeverity, name='chart'),
     path('stations', views.map_station, name='map_station'),
 ]
