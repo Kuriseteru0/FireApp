642e9a771c91e6f65c110259d76708f20b62359d
Kuriseteru0 <115957758+Kuriseteru0@users.noreply.github.com>
2025-04-15 11:39:00 +0800

    fire incident edit

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('lineChart/', LineCountByMonth, name='chart'),
     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
     path('multiBarChart/', multipleBarbySeverity, name='chart'),
-    path('stations', views.map_station, name='map_station'),
+    path('map_stations', views.map_station, name='map_station'),
     path('fireIncidents', views.map_fire_incidents, name='map_fire_incidents'),
+    path('maps/jqvmap.html', views.jqvmap_view, name='jqvmap'),  # Add this line
 ]
